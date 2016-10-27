9248e9862f667bfc44576fca819fdb2736dd4085
jiayuqiang <jiayuqiang@3wchina.net>
2016-10-27 10:03:06 +0800

    Initial Commit

---
+++ SlideViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideViewController
+//
+//  Created by 朱前进 on 16/10/27.
+//  Copyright © 2016年 com.3wchina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlideViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideViewController
+//
+//  Created by 朱前进 on 16/10/27.
+//  Copyright © 2016年 com.3wchina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
